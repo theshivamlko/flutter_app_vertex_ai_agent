e5f3fc39ce653b358ebbe7bca8e7dc5fb32f673e
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2025-05-25 01:52:21 +0530

    text base chat

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,20 @@ import 'package:flutter/material.dart';
 
 import 'my_home_page.dart';
 
-void main() async{
-
-    const String androidApiKey = String.fromEnvironment('androidApiKey', defaultValue: 'default_key');
-
-
+void main() async {
+  const String androidApiKey = String.fromEnvironment(
+    'androidApiKey',
+    defaultValue: 'default_key',
+  );
 
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: FirebaseOptions(apiKey: androidApiKey,
-          appId: "1:542049090470:android:6a5f61d4091fb47c946d67",
-          messagingSenderId: "542049090470", projectId: "starlit-primacy-460716-t2")
+    options: FirebaseOptions(
+      apiKey: androidApiKey,
+      appId: "1:542049090470:android:6a5f61d4091fb47c946d67",
+      messagingSenderId: "542049090470",
+      projectId: "starlit-primacy-460716-t2",
+    ),
   );
 
 
@@ -29,11 +32,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-
--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,11 +1,8 @@
-
-
-
+import 'package:firebase_ai/firebase_ai.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
@@ -14,43 +11,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late GenerativeModel model;
 
+  ValueNotifier<List<String>> chatMessages = ValueNotifier<List<String>>([]);
+
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    model = FirebaseAI.vertexAI().generativeModel(
+      model: 'gemini-2.0-flash-001',
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: ValueListenableBuilder(
+          valueListenable: chatMessages,
+          builder: (context, value, child) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: textEditingController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1),
+                      ),
+                      suffix: IconButton(
+                        onPressed: () {
+                          submitText(textEditingController.text);
+                        },
+                        icon: Icon(Icons.send),
+                      ),
+                    ),
+                    onSubmitted: (value) {
+                      submitText(value);
+                    },
+                  ),
+                ),
+                ...List.generate(value.length, (index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(value[index], style: TextStyle(fontSize: 18,color: index%2==0?Colors.black:Colors.deepOrange),),
+                        Divider()
+                      ],
+                    ),
+                  );
+                }),
+              ],
+            );
+          },
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void submitText(String text) async {
+    FocusScope.of(context).unfocus();
+    textEditingController.text = "";
+
+    final content = [Content.text(text)];
+    chatMessages.value=[...chatMessages.value,text];
+
+    final response = await model.generateContent(content);
+
+    print("response ${response.text}");
+    chatMessages.value=[...chatMessages.value,response.text ?? "N/A"];
+
   }
 }
